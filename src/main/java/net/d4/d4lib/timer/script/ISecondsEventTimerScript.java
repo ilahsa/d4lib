1483a46787a93a09e84ad0204106d3315687ff0d
baibaoqing <ilahsa@126.com>
2017-02-21 10:49:38 +0800

    update

--- src/main/java/net/d4/d4lib/timer/script/ISecondsEventTimerScript.java
+++ src/main/java/net/d4/d4lib/timer/script/ISecondsEventTimerScript.java
@@ -4,10 +4,6 @@ import net.d4.d4lib.scriptpool.IBaseScript;
 
 /**
  *
- *
- * @author 失足程序员
- * @mail 492794628@qq.com
- * @phone 13882122019
  */
 public interface ISecondsEventTimerScript extends IBaseScript {
 
